1ed3abd295ec9d7b7c75c6eb8249333b335c6406
taylorchu <march@localhost.localdomain>
2012-05-06 13:42:23 -0700

    suppress warning; start to build

--- src/edu/ucla/loni/server/FileServiceImpl.java
+++ src/edu/ucla/loni/server/FileServiceImpl.java
@@ -14,6 +14,7 @@ import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
